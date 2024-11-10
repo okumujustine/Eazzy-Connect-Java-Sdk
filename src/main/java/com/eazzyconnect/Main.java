4231cfc2ebdef857ed36eed4825c05062ea3d6e8
Justine Okumu <okumujustine01@gmail.com>
2024-11-09 21:51:20 -0600

    update documentation

--- src/main/java/com/eazzyconnect/Main.java
+++ src/main/java/com/eazzyconnect/Main.java
@@ -1,20 +1,24 @@
 package com.eazzyconnect;
 
+import com.eazzyconnect.sms.EazzySms;
 import com.eazzyconnect.wallet.EazzyWallet;
 
 public class Main {
     public static void main(String[] args) {
 
         // SMS actions
-//        String singleContact = "+256781459239";
-//        EazzyApiResponse singleSms = EazzySms.sendSms(singleContact, "fix JAVA SDK single sms send");
-//        System.out.println(singleSms);
 
-//        String[] bulkContacts = {"+256781459239", "+256787584128"};
-//        EazzyApiResponse bulkSms = EazzySms.sendBulkSms(bulkContacts, "fix JAVA SDK bulk sms send");
-//        System.out.println(bulkSms);
+        // single
+        String singleContact = "+256781459239";
+        EazzyApiResponse singleSms = EazzySms.sendSms(singleContact, "fix JAVA SDK single sms send");
+        System.out.println(singleSms);
 
-//         Wallet actions
+        // bulk
+        String[] bulkContacts = {"+256781459239", "+256787584128"};
+        EazzyApiResponse bulkSms = EazzySms.sendBulkSms(bulkContacts, "fix JAVA SDK bulk sms send");
+        System.out.println(bulkSms);
+
+        // Wallet actions
         EazzyApiResponse balance = EazzyWallet.getBalance("WL-00004");
         System.out.println(balance);
     }
